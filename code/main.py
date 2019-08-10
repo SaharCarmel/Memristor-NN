c4d86ae038d51238d0c69a4db783f4ffac9df18c
AmirsSaad <45436875+AmirsSaad@users.noreply.github.com>
2019-08-10 22:21:01 +0300

    prg

--- code/main.py
+++ code/main.py
@@ -11,52 +11,58 @@ from matplotlib import pyplot as plt
 import torchvision.datasets as datasets
 os.chdir('code')
 import nn_modules
+import torchvision.transforms as transforms
 
 #yamlFile = sys.argv[1]
 
 ## Checking if cuda available
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
+# use_cuda = torch.cuda.is_available()
+# device = torch.device("cuda" if use_cuda else "cpu")
 
 yamlFile= 'Parameters.yml'
-def load_param(yamlfile,version,parm_name):
-        version = 'version' + str(version)
-        with open(yamlfile) as f:
-                data = yaml.load(f, Loader=yaml.FullLoader)
-                return data[version][parm_name]
+# def load_param(yamlfile,version,parm_name):
+#         version = 'version' + str(version)
+#         with open(yamlfile) as f:
+#                 data = yaml.load(f, Loader=yaml.FullLoader)
+#                 return data[version][parm_name]
 
 ## Loading parameter
-batchSize = load_param(yamlFile,1,'batchSize')
+# batchSize = load_param(yamlFile,1,'batchSize')
+batchSize = 10
 
+
+transform = transforms.Compose(
+    [transforms.ToTensor()])
 ## Loading datasets
-trainset = datasets.MNIST('./data',train=True,transform=None,download=True)
-train_loader = torch.utils.data.DataLoader(trainset,
+trainset = datasets     .MNIST('./data',train=True,transform=transform,download=True)
+trainloader = torch.utils.data.DataLoader(trainset,
         batch_size=batchSize, shuffle=True)
 
-testset = datasets.MNIST('./data',train=False,transform=None,download=True)
+testset = datasets.MNIST('./data',train=False,transform=transform,download=True)
 testLoader = torch.utils.data.DataLoader(testset,
         batch_size=batchSize, shuffle=True)
 
 
-model = nn_modules.manhattan_net().double()
+model = nn_modules.base_net().float()
 
 #binary cross entropy loss
 criterion = nn.CrossEntropyLoss()
 #optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-permutation = torch.randperm(x_train.size()[0])
-
+print(trainloader)
 train_loss = list()
 train_acc = list()
 test_loss = list()
 test_acc = list()
-#training
-for epoch in range(100):
-    #print ("Epoch: "+str(epoch))
-    for i in range(0,x_train.size()[0],batch_size):
+
+for epoch in range(2):
+    print ("Epoch: "+str(epoch))
+    for i , data in enumerate(trainloader,0):
         #print ("batch: "+str(i))
-        indices = permutation[i:i+batch_size]
-        inputs, labels = x_train[indices], y_train[indices]
+        
+        inputs, labels = data
+        inputs = inputs.view(-1,28*28)
+        print(inputs.shape)
         outputs = model(inputs)
 
         loss=criterion(outputs,labels)
@@ -64,24 +70,25 @@ for epoch in range(100):
         
         model.zero_grad()
         loss.backward()
-        # model.fc_pos.weight.data -= 0.005*np.sign(model.fc_pos.weight.grad)
-        # model.fc_neg.weight.data -= 0.005*np.sign(model.fc_neg.weight.grad)
+        
 
-        model.update_weights()
+        model.update_weights(eta=0.005)
                 
         train_loss.append(loss.item())
 
-        out = model(x_train)
-        _, predicted = torch.max(out.data, 1)
-        train_acc.append((predicted == y_train).sum().item()/y_train.size(0))
+        # out = model(x_train)
+#         # _, for epoch in range(2):  # loop over the dataset multiple times
+
+
+#         # train_acc.append((predicted == y_train).sum().item()/y_train.size(0))
         
-        out = model(x_test)
-        test_loss.append(criterion(out,y_test).item())
-        _, predicted = torch.max(out.data, 1)
-        test_acc.append((predicted == y_test).sum().item()/y_test.size(0))
+#         # out = model(x_test)
+#         # test_loss.append(criterion(out,y_test).item())
+#         # _, predicted = torch.max(out.data, 1)
+#         # test_acc.append((predicted == y_test).sum().item()/y_test.size(0))
         
 print('train loss', train_loss[-1])        
-print('test loss', test_loss[-1])
-print('train accuracy', train_acc[-1])
-print('test accuracy', test_acc[-1])
+# # print('test loss', test_loss[-1])
+# # print('train accuracy', train_acc[-1])
+# # print('test accuracy', test_acc[-1])
 
