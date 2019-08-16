5bb328d850a1c79b356e1fdd115b5a96b096af20
AmirsSaad <45436875+AmirsSaad@users.noreply.github.com>
2019-08-16 22:03:09 +0300

    added Memristor layer

--- nn_modules.py
+++ nn_modules.py
@@ -3,35 +3,91 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-class base_net(torch.nn.Module):
+
+# import math
+from torch.nn.parameter import Parameter
+import torch.nn.init as init
+
+
+class ref_net(torch.nn.Module):
+    # refernce model see wikipedia MNIST or
+    # http://yann.lecun.com/exdb/mnist/
+    # 2-layer NN, 800 HU, Cross-Entropy Loss
     def __init__(self):
-        super(base_net, self).__init__()
+        super(ref_net, self).__init__()
         self.fc1 = nn.Linear(784, 800)
         self.fc2 = nn.Linear(800, 10)
+        self.criterion = nn.CrossEntropyLoss(reduction='sum')
 
     def forward(self, x):
         x = x.view(-1,28*28)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
-
         #x = F.softmax(self.fc(x),0)
         return x
     
-    def update_weights(self,eta):
-         self.fc1.weight.data -= eta*self.fc1.weight.grad
-         self.fc2.weight.data -= eta*self.fc2.weight.grad
+    def update_weights(self,lr):
+         self.fc1.weight.data -= lr*self.fc1.weight.grad
+         self.fc2.weight.data -= lr*self.fc2.weight.grad
 
 class manhattan_net(torch.nn.Module):
     def __init__(self):
         super(manhattan_net, self).__init__()
-        self.fc_pos = nn.Linear(4, 3)
-        self.fc_neg = nn.Linear(4, 3)
+        
+        self.fc1_pos = Memristor_layer(784, 800)
+        self.fc1_neg = Memristor_layer(784, 800)
+        self.fc2_pos = Memristor_layer(800, 10)
+        self.fc2_neg = Memristor_layer(800, 10)
+        
+        self.criterion = nn.CrossEntropyLoss(reduction='sum')
 
     def forward(self, x):
-        x = self.fc_pos(x) - self.fc_neg(x)
+        x = x.view(-1,28*28)
+        x = F.relu(self.fc1_pos(x)-self.fc1_neg(x))
+        x = self.fc2_pos(x) - self.fc2_neg(x)
         #x = F.softmax(self.fc(x),0)
         return x
 
-    def update_weights(self,eta):
-        self.fc_pos.weight.data -= eta*np.sign(self.fc_pos.weight.grad)
-        self.fc_neg.weight.data -= eta*np.sign(self.fc_neg.weight.grad)
+    def update_weights(self,lr):
+        self.fc1_pos.update_weight(lr)
+        self.fc1_neg.update_weight(lr)
+        self.fc2_pos.update_weight(lr)
+        self.fc2_neg.update_weight(lr)
+
+
+
+class Memristor_layer(torch.nn.Module):
+    __constants__ = ['bias', 'in_features', 'out_features']
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(Memristor_layer, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(out_features, in_features))
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight, a=np.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / np.sqrt(fan_in)
+            init.uniform_(self.bias, 0, 2*bound)
+
+    def forward(self, input):
+        return F.linear(input, self.weight, self.bias)
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
+    
+    def update_weight(self,lr,lower=0,upper=1):
+        self.weight.data -= lr*torch.sign(self.weight.grad)
+        self.weight.data.requires_grad = False
+        self.weight.data[self.weight.data<lower] = lower
+        self.weight.data[self.weight.data>upper] = upper
+        self.weight.data.requires_grad = True
--- script.py
+++ script.py
@@ -1,9 +1,5 @@
-from __future__ import print_function
 import argparse
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 import yaml
 from torchvision import datasets, transforms
 import nn_modules
@@ -11,34 +7,40 @@ from nn_utils import Args , train , test
 
 
 # Training settings
-args = Args('Parameters.yml')
-use_cuda = not args.noCude and torch.cuda.is_available()
+def main():
+    args = Args('Parameters.yml')
+    use_cuda = not args.noCude and torch.cuda.is_available()
 
-torch.manual_seed(args.seed)
+    torch.manual_seed(args.seed)
 
-device = torch.device("cuda" if use_cuda else "cpu")
+    device = torch.device("cuda" if use_cuda else "cpu")
 
-kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                    transform=transforms.Compose([
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.1307,), (0.3081,))
-                    ])),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.1307,), (0.3081,))
-                    ])),
-    batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                        transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.1307,), (0.3081,))
+                        ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.1307,), (0.3081,))
+                        ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-model = nn_modules.base_net().to(device)
-criterion = nn.CrossEntropyLoss(reduction='sum')
+    model = nn_modules.manhattan_net().to(device)
+    # criterion = torch.nn.CrossEntropyLoss(reduction='sum')
 
-for epoch in range(1, args.epochs + 1):
-    train(args, model, device, train_loader, criterion , epoch)
-    test(args, model, device, test_loader, criterion)
+    for epoch in range(1, args.epochs + 1):
+        args.lr /= (10**(epoch-1))
+        print(args.lr)
+        train(args, model, device, train_loader, model.criterion , epoch)
+        test(args, model, device, test_loader, model.criterion)
 
-if (args.save_model):
-    torch.save(model.state_dict(),"mnist_cnn.pt")
+    if (args.save_model):
+        torch.save(model.state_dict(),"mnist_cnn.pt")
+
+if __name__ == '__main__':
+    main()
