aa5f42c5227fa60c09269bd3630658e7f1097ce0
Sahar Carmel <saharc@Levitection.Local>
2019-08-16 13:44:47 +0300

    Added doundations for using sacred

--- script.py
+++ script.py
@@ -8,37 +8,54 @@ import yaml
 from torchvision import datasets, transforms
 import nn_modules
 from nn_utils import Args , train , test
+from sacred import Experiment
+from sacred.observers import MongoObserver
+from sacred.utils import apply_backspaces_and_linefeeds
+
+
+ex = Experiment()
+ex.observers.append(MongoObserver.create())
+ex.captured_out_filter = apply_backspaces_and_linefeeds
 
 
 # Training settings
-args = Args('Parameters.yml')
-use_cuda = not args.noCude and torch.cuda.is_available()
+@ex.config
+def my_config():
+    args = Args('Parameters.yml')
 
-torch.manual_seed(args.seed)
 
-device = torch.device("cuda" if use_cuda else "cpu")
+@ex.automain
+def main(args):
+    use_cuda = not args.noCude and torch.cuda.is_available()
 
-kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                    transform=transforms.Compose([
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.1307,), (0.3081,))
-                    ])),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.1307,), (0.3081,))
-                    ])),
-    batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    torch.manual_seed(args.seed)
 
-model = nn_modules.base_net().to(device)
-criterion = nn.CrossEntropyLoss(reduction='sum')
+    device = torch.device("cuda" if use_cuda else "cpu")
 
-for epoch in range(1, args.epochs + 1):
-    train(args, model, device, train_loader, criterion , epoch)
-    test(args, model, device, test_loader, criterion)
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                        transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.1307,), (0.3081,))
+                        ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.1307,), (0.3081,))
+                        ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-if (args.save_model):
-    torch.save(model.state_dict(),"mnist_cnn.pt")
+    model = nn_modules.base_net().to(device)
+    criterion = nn.CrossEntropyLoss(reduction='sum')
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, criterion , epoch)
+        test(args, model, device, test_loader, criterion)
+
+    if (args.save_model):
+        torch.save(model.state_dict(),"mnist_cnn.pt")
+
+if __name__ == '__main__':
+    main()
