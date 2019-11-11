9ed02daf568ff9bdf2d98dfc05aaba6f912b22e9
AmirsSaad <45436875+AmirsSaad@users.noreply.github.com>
2019-11-12 01:49:52 +0200

    phys values

--- run_script.py
+++ run_script.py
@@ -2,14 +2,16 @@ from nn_experiments import ex
 from nn_utils import visualize_digitization, Args
 
 def main():
-    ex.run(config_updates={'net': 'ref_net','digitizeInput':True}, options={'--name': 'phase_1'})
-    ex.run(config_updates={'net': 'ref_net','digitizeInput':True}, options={'--name': 'phase_1'})
-    ex.run(config_updates={'net': 'manhattan_net','digitizeInput':False}, options={'--name': 'phase_1'})
-    ex.run(config_updates={'net': 'manhattan_net','digitizeInput':True}, options={'--name': 'phase_1'})
+    # ex.run(config_updates={'net': 'ref_net','digitizeInput':True}, options={'--name': 'phase_1'})
+    # ex.run(config_updates={'net': 'ref_net','digitizeInput':True}, options={'--name': 'phase_1'})
+    # ex.run(config_updates={'net': 'manhattan_net','lower':0,'upper':1,'digitizeInput':False}, options={'--name': 'phase_1'})
+    # ex.run(config_updates={'net': 'manhattan_net','lower':1e3,'upper':1e3+1,'digitizeInput':False}, options={'--name': 'phase_1'})
+    ex.run(config_updates={'net': 'manhattan_net','lower':1/(1.45e6),'upper':1/(1.45e3),'digitizeInput':False}, options={'--name': 'phase_1'})
+    # ex.run(config_updates={'net': 'manhattan_net','digitizeInput':True}, options={'--name': 'phase_1'})
 
 
-    args = Args('Parameters.yaml')
-    visualize_digitization(args)
+    # args = Args('Parameters.yaml')
+    # visualize_digitization(args)
 
 if __name__ == '__main__':
     main()
